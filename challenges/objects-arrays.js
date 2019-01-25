3510197883c39a40e0e6a4e8d2fdb295d4a226bf
Kim Duclos <kimduclos@gmail.com>
2019-01-25 09:11:07 -0700

    created dino objects

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -15,20 +15,43 @@
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
 
 // What was the diet of a velociraptor?
-console.log();
 
 // How long was a stegosaurus?
-console.log();
 
 // What time period did tyrannosaurus live in?
-console.log();
-
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+
+const dino1 = {
+  name: "tyrannosaurus",
+  diet: "carnivorous",
+  weight: "7000kg",
+  length: "12m",
+  period: "Late Cretaceious", 
+  roar: function() {
+     return ("RAWERSRARARWERSARARARRRR!");
+  }
+};
+
+const dino2 = {
+  name: "stegosaurus",
+  diet: "herbivorous",
+  weight: "2000kg",
+  length: "9m",
+  period: "Late Cretaceious" 
+};
+
+
+const dino3 = {
+  name: "velociraptor",
+  diet: "carnivorous",
+  weight: "15kg",
+  length: "1.8m",
+  period: "Late Cretaceious" 
+};
+
 
 
 // ==== Arrays ====
